08c46179aff3446f9506c048b51a63fca378fbd3
RsRuh <rshridoysr@gmail.com>
2022-12-16 12:00:57 -0700

    add env

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://PowPow:k0otOvfo8eyJJO6g@cluster0.v8gjvac.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.v8gjvac.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
